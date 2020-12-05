9f4ae32f214cfb6725090f487a45af9c47dd9e24
MahmoudFettal <MahmoudFettal@users.noreply.github.com>
2020-12-05 12:44:29 +0100

    version 1.0

--- Minesweeper/Grid.py
+++ Minesweeper/Grid.py
@@ -3,12 +3,13 @@ import pygame, os
 
 class Settings():
     def __init__(self, lines, columns):
-        self.lines   = lines
-        self.columns = columns
-        self.screen_size = (105*self.lines, 105*self.columns) 
-        self.frames = ['U'] + list(map(str,range(9)))
-        print(self.frames)        
-        self.frames      = {frame : pygame.image.load(os.path.join('frames',f'{frame}.png')) for frame in ['U'] + list(map(str,range(9)))}
+        self.lines       = lines
+        self.columns     = columns
+        self.screen_size = (25*self.lines + 20, 25*self.columns + 60) 
+        self.frames      = {frame : pygame.image.load(os.path.join('frames',f'{frame}.png')) for frame in ['U','M','F','playing','lost','won'] + list(map(str,range(9)))}
+        self.flaged      = False
+        self.width       = 25
+        self.bg_color    = pygame.Color('#A1A1A1')
 
 
 class Cell():
@@ -16,23 +17,28 @@ class Cell():
         self.contact = 0 # the number of mines in contacts with this cell
         self.mine    = False
         self.covered = False 
+        self.flaged  = False
     def __repr__(self):
         if self.covered:
             return str(self.contact)
+        elif self.flaged:
+            return 'F'
         return 'U'
 
 class Grid():
     def __init__(self, lines, columns, n_mines):
-        self.lines   = lines
-        self.columns = columns
-        self.n_mines = n_mines # the number of mines
-        self.r_cells = lines*columns - n_mines # the number of the remainging free cells
-        self.grid    = [[Cell() for _ in range(columns)] for _ in range(lines)]
-        self.zeros   = []
-        self.over    = False
-        self.coords  = [[-1, -1],[-1, 0], [-1, 1],
-                        [ 0, -1],         [0, 1 ],
-                        [ 1, -1], [1, 0], [1, 1 ]]
+        self.lines    = lines
+        self.columns  = columns
+        self.n_mines  = n_mines # the number of mines
+        self.r_cells  = lines*columns - n_mines # the number of the remainging free cells
+        self.grid     = [[Cell() for _ in range(columns)] for _ in range(lines)]
+        self.zeros    = []
+        self.over     = False
+        self.coords   = [[-1, -1],[-1, 0], [-1, 1],
+                         [ 0, -1],         [0, 1 ],
+                         [ 1, -1], [1, 0], [1, 1 ]]
+        self.won      = False
+        self.c_flaged = 0
 
     def fill_mines(self):
         self.mines   = []
@@ -63,27 +69,16 @@ class Grid():
                 for a,b in self.coords:
                     if 0 <= a+i < self.lines and 0 <= b+j < self.columns and not self.grid[a+i][b+j].mine and not (a+i,b+j) in self.zeros:
                         self.uncover(a+i, b+j)
-    
+
     def make_move(self,x,y):
-        '''while True:
-            x,y = map(int, input().split())
-            if 0 <= x < self.lines and 0 <= y < self.columns and not self.grid[x][y].covered:
-                break  '''
         if self.grid[x][y].mine:
             self.over = True
         else:    
             self.uncover(x,y)
-
-    def show(self):
-        #self.mines_contact()
-        #print('\n'.join([' '.join([str(j.contact) for j in i]) for i in self.grid]))    
-        #print('\n'.join([' '.join(list(map(str,i))) for i in self.grid])) 
-        #while not self.over:
-            #self.make_move()
-        print('\n'.join([' '.join(list(map(str,i))) for i in self.grid])) 
-        #print('game over')
+            if self.r_cells == 0:
+                self.won = True
+                self.c_flaged = self.n_mines
  
 if __name__ == "__main__":
-    game = Grid(5,5,5)
+    game = Grid(9,9,10)
     game.fill_mines()
-    game.show()
--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -10,36 +10,93 @@ class Game():
         self.lines    = lines
         self.columns  = columns
         self.settings = Settings(self.lines,self.columns)
+        self.mines    = mines
 
     def update(self,screen):
         for i in range(self.grid.lines):
             for j in range(self.grid.columns):
-                screen.blit(self.settings.frames[str(self.grid.grid[i][j])], (j*105,i*105))
+                screen.blit(self.settings.frames[str(self.grid.grid[i][j])], (10 + j*self.settings.width, 50 + i*self.settings.width))   
+        screen.blit(self.settings.frames['playing'], (10+(self.grid.columns-1)*self.settings.width, 12))        
+        self.display_score(screen)
         clock = pygame.time.Clock()
         clock.tick(5)
         pygame.display.flip()
-        
+
+    def game_over(self,screen):
+        for i in range(self.grid.lines):
+            for j in range(self.grid.columns):
+                if [i,j] in self.grid.mines:
+                    screen.blit(self.settings.frames['M'], (10 + j*self.settings.width,50 + i*self.settings.width))
+                else:
+                    screen.blit(self.settings.frames[str(self.grid.grid[i][j].contact)], (10 + j*self.settings.width,50 + i*self.settings.width))
+        screen.blit(self.settings.frames['lost'], (10+(self.grid.columns-1)*self.settings.width, 12))                
+        clock = pygame.time.Clock()
+        clock.tick(5)
+        pygame.display.flip()
+
+    def game_won(self,screen):
+        for i in range(self.grid.lines):
+            for j in range(self.grid.columns):
+                if [i,j] in self.grid.mines:
+                    screen.blit(self.settings.frames['M'], (10 + j*self.settings.width,50 + i*self.settings.width))
+                else:
+                    screen.blit(self.settings.frames[str(self.grid.grid[i][j].contact)], (10 + j*self.settings.width,50 + i*self.settings.width))
+        screen.blit(self.settings.frames['won'], (10+(self.grid.columns-1)*self.settings.width, 12))                
+        clock = pygame.time.Clock()
+        clock.tick(5)
+        pygame.display.flip()
+
     def get_move(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 n,m = pygame.mouse.get_pos()
-                self.grid.make_move(m//105,n//105)
-                self.grid.show()
+                if 50 < m < 50+(self.grid.columns)*self.settings.width and 10 < n < 10 + (self.grid.columns)*self.settings.width:
+                    self.grid.make_move((m-50)//self.settings.width,(n-10)//self.settings.width)
+                elif 10 + (self.grid.columns-1)*self.settings.width <= n <= (self.grid.columns-1)*self.settings.width + 36 and 12 <= m < 38:
+                    self.grid = Grid(self.lines, self.columns, self.mines)
+                    self.play()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                n,m = pygame.mouse.get_pos()
+                if m > 50 or m < (self.grid.columns)*self.settings.width or n > 10 or n < (self.grid.columns)*self.settings.width:
+                    if self.grid.grid[(m-50)//self.settings.width][(n-10)//self.settings.width].flaged:
+                        self.grid.grid[(m-50)//self.settings.width][(n-10)//self.settings.width].flaged = False
+                        self.grid.c_flaged -= 1
+                    else:
+                        self.grid.grid[(m-50)//self.settings.width][(n-10)//self.settings.width].flaged = True
+                        self.grid.c_flaged += 1
+
+    def display_score(self,screen):
+        screen.blit(pygame.image.load(os.path.join('frames','block.png')), (10, 12))
+        num = str(self.grid.n_mines - self.grid.c_flaged)
+        if len(num) == 1:
+            screen.blit(pygame.image.load(os.path.join('frames', 'numbers', '0.png')), (19, 18))
+            screen.blit(pygame.image.load(os.path.join('frames', 'numbers', f'{num}.png')), (34, 18))
+        else:
+            screen.blit(pygame.image.load(os.path.join('frames', 'numbers', f'{num[0]}.png')), (19, 18))
+            screen.blit(pygame.image.load(os.path.join('frames', 'numbers', f'{num[1]}.png')), (34, 18))
 
     def play(self):
         pygame.init()
         pygame.display.set_caption('minesweeper')
         screen = pygame.display.set_mode(self.settings.screen_size)
+        screen.fill(self.settings.bg_color)
         self.grid.fill_mines()
         self.grid.mines_contact()
         self.update(screen)
         while True:
             self.get_move()
             self.update(screen)
-            
+            if self.grid.over:
+                self.game_over(screen)
+                while True:
+                    self.get_move()
+            elif self.grid.won:
+                self.game_won(screen)
+                while True:
+                    self.get_move()            
 
 if __name__ == "__main__":
-    game = Game(5,5,5)
+    game = Game(16,16,40)
     game.play()
